34512c7f41e8d0d5c0515a9175fca2bdacdc3803
samadchutiya <samad.hussain4455@gmail.com>
2019-11-20 01:13:33 +0530

    first commitDemo.java

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample{
+	public static void main(String[] args)
+{
+		
+	for (int i=1; i<=10;i++)
+
+{
+			System.out.println(" My Name is Bala..." +i);
+}
+}
+}
---
+++ mnptest.java
@@ -0,0 +1,5 @@
+class mnptest{  
+    public static void main(String args[]){  
+     System.out.println("Hello World");  
+    }  
+}  
